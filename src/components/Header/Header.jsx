e9f792d10cc6a8654aa0edca40ab4499cf9e0d9a
hironmoy-ray <hpray35@gmail.com>
2024-08-24 18:58:10 +0600

    Complete Nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { RiMenu2Fill } from "react-icons/ri";
 const Header = () => {
   const [open, setOpen] = useState(true);
   return (
-    <nav className="relative md:flex md:w-full md:justify-center bg-rose-700 text-white">
+    <nav className="relative md:flex md:w-full md:justify-center bg-rose-700 text-white h-12">
       <div
         onClick={() => {
           setOpen(!open);
@@ -16,7 +16,7 @@ const Header = () => {
       <ul className="hidden md:block md:flex-1 ">
         <li className="px-6 py-1 md:my-2 rounded-xl">
           <a
-            className="hover:border-white hover:border-2 rounded px-2 py-1 duration-100"
+            className="hover:border-white hover:border-2 rounded px-2 py-1 duration-100 font-bold text-nowrap"
             href="#"
           >
             Comp Name
@@ -26,20 +26,23 @@ const Header = () => {
 
       <ul
         className={`flex  flex-col md:flex-row md:items-center md:justify-center px-4 mx-4 rounded-xl md:rounded-none md:mx-0 text-center bg-rose-700 text-white absolute md:static duration-1000 ${
-          open ? "top-16" : "-top-36"
+          open ? "top-16" : "-top-44"
         }`}
       >
-        <li className="hover:border-b-white hover:border-b-2 mx-2 px-6 py-1 md:my-2 rounded-xl duration-100">
+        <li className="hover:border-b-white hover:border-b-2 mx-2 px-6 py-1 font-semibold md:my-2 rounded-xl duration-100">
           <a href="#">Home</a>
         </li>
-        <li className="hover:border-b-white hover:border-b-2 mx-2 px-6 py-1 md:my-2 rounded-xl duration-100">
+        <li className="hover:border-b-white hover:border-b-2 mx-2 px-6 py-1 font-semibold md:my-2 rounded-xl duration-100">
           <a href="#user">Users</a>
         </li>
-        <li className="hover:border-b-white hover:border-b-2 mx-2 px-6 py-1 md:my-2 rounded-xl duration-100">
+        <li className="hover:border-b-white hover:border-b-2 mx-2 px-6 py-1 font-semibold md:my-2 rounded-xl duration-100">
           <a href="#contact">Contact</a>
         </li>
-        <li className="hover:border-b-white hover:border-b-2 mx-2 px-6 py-1 my-1 md:my-2 rounded-xl duration-100">
+        <li className="hover:border-b-white hover:border-b-2 mx-2 px-6 py-1 font-semibold md:my-2 rounded-xl duration-100">
           <a href="#about">About</a>
+        </li>
+        <li className="hover:border-b-white md:hidden hover:border-b-2 mx-2 px-6 py-1 my-1 font-semibold md:my-2 rounded-xl duration-100">
+          <a href="#profile">Profile</a>
         </li>
       </ul>
 
@@ -47,7 +50,7 @@ const Header = () => {
         <li className=" px-6 py-1 md:my-2 rounded-xl">
           <a
             className="hover:border-white hover:border-2 rounded px-2 py-1 duration-100"
-            href="#"
+            href="#profile"
           >
             Profile
           </a>
